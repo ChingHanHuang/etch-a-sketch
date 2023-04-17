69bdb74feca6f5a18882373af9f32d8a54b05c71
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-17 17:26:43 +0800

    Add cell change color function
    First, set document body mouse down value, when click cell, this value will be true, otherwise, false.
    Second, change cell color if mouse is down and over cell.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const DEFAULT_SIZE = 10;
 
+let mousedown = false;
+document.body.onmousedown = () => mousedown = true;
+document.body.onmouseup = () => mousedown = false;
+
 
 const clearBtn = document.getElementById("clearBtn");
 clearBtn.addEventListener('click', clearPaint);
@@ -23,11 +27,13 @@ function setUpGrid(size) {
         cell.style.borderColor = "blue";
         cell.style.borderStyle = "solid";
         cell.addEventListener("mousedown", changeColor);
+        cell.addEventListener("mouseover", changeColor);
         grid.appendChild(cell);
     }
 }
 
 function changeColor(e) {
+    if (e.type === "mouseover" && !mousedown) return;
     e.target.style.backgroundColor = "red";
 }
 
