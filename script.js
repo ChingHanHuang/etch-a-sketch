55ce65d68e4e3047cdc39539eac1fcab8d5b5d71
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-17 18:55:16 +0800

    Add clear grid button function

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 const DEFAULT_SIZE = 10;
-const DEFAULT_COLOR = "";
+const DEFAULT_GRID_COLOR = "lightseagreen";
+const DEFAULT_PICK_COLOR = "#ff99cc";
 
-let currentColor = DEFAULT_COLOR;
+let currentColor = DEFAULT_PICK_COLOR;
+let currentSize;
 let mousedown = false;
 document.body.onmousedown = () => mousedown = true;
 document.body.onmouseup = () => mousedown = false;
 
 const colorPicker = document.getElementById("colorPicker");
+const sizeValue = document.getElementById("size");
+const slider = document.getElementById("slider");
 const clearBtn = document.getElementById("clearBtn");
+const grid = document.getElementById("grid");
 
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
-clearBtn.onclick = () => clearGridColor();
+clearBtn.onclick = () => reloadGrid();
+slider.oninput = (e) => changeSize(e.target.value);
 
 
 function setCurrentColor(newColor) {
     currentColor = newColor;
 }
 
-let slider = document.getElementById("myRange");
-slider.oninput = function() {
-    document.querySelectorAll(".cell").forEach(el => el.remove());
-    setUpGrid(this.value);
-    updateSizeValue(this.value);
+function setCurrentSize(newSize) {
+    currentSize = newSize;
 }
 
-function setUpGrid(size) {
-    let grid = document.getElementById("grid");
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+function reloadGrid() {
+    clearGrid();
+    setUpGrid();
+}
 
-    for(let i = 0; i < size * size; i++) {
+function changeSize(size) {
+    setCurrentSize(size);
+    updateSizeValue();
+    reloadGrid()
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+function setUpGrid() {
+    grid.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+    grid.style.backgroundColor = DEFAULT_GRID_COLOR;
+
+    for(let i = 0; i < currentSize * currentSize; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.style.borderColor = "blue";
+        cell.style.borderColor = "grey";
         cell.style.borderStyle = "solid";
         cell.addEventListener("mousedown", changeColor);
         cell.addEventListener("mouseover", changeColor);
@@ -45,15 +63,11 @@ function changeColor(e) {
     e.target.style.backgroundColor = currentColor;
 }
 
-function updateSizeValue(size) {
-    document.getElementById("size").innerHTML = `${size} x ${size}`;
-
-}
-
-function clearGridColor() {
-
+function updateSizeValue() {
+    sizeValue.innerHTML = `${currentSize} x ${currentSize}`;
 }
 
 window.onload = () => {
-    setUpGrid(DEFAULT_SIZE);
+    setCurrentSize(DEFAULT_SIZE);
+    setUpGrid();
 }
