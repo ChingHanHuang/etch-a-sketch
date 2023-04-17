35ea1d54a99498b203e121f0cac5f24cf50d7b0d
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-17 15:22:42 +0800

    Initialize the header content and main content
    Add header "Etch-a-Sketch"
    
    Create a defaule size grid with value 5 for each side.
    
    Create a slider, when change the value of slider, remove original grid and create new grid with new size value, according to the slider value.

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+let slider = document.getElementById("myRange");
+slider.oninput = function() {
+    document.querySelectorAll(".column").forEach(el => el.remove());
+    createGrid(this.value);
+}
+
+function createGrid(size) {
+    let grid = document.getElementById("grid");
+    for(let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.className = "column";
+        for(let j = 0; j < size; j++) {
+            let row = document.createElement("div");
+            row.className = "row";
+            row.style.width = "50px";
+            row.style.height = "50px";
+            row.style.background = "pink";
+
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }
+}
+
+createGrid(slider.value);
